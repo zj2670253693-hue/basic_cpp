a07a3b0d5c930e1496bdf5f6b9ef3857fc1ca32d
huawei <zj2670253693@gmail.com>
2026-05-31 17:11:50 +0800

    构造and析构

--- const_test.cpp
+++ const_test.cpp
@@ -9,12 +9,29 @@ using namespace std;
 // 在出++中 b必须初始化，叫做常量
 
 int main() {
-    const int a = 10;
-    int *p = (int *)&a;
-    *p = 100;
+    //const int a = 10;
+    // int *p = (int *)&a;
+    //*p = 100;
     // 在clion中，编译器没有进行优化，所以这里*(&a) 也是100
     // 这里就相当于&a拿到了a的地址，然后通过地址获取到了a的值，所以*(&a) 也是100
     // 但是在vs上，编译器进行了优化，所以这里*(&a) 不是100，而是100
-    printf("%d %d %d\n",a,*p,*(&a));
+    //printf("%d %d %d\n",a,*p,*(&a));
+    //int num = 20;
+    //const int *ptr = &num;
+    // 把限制大的转换成限制小的是不行的，只能把 限制小的转化为限制大的
+    // 只能把 const int* <= int *
+    // 在这里是把 int * <= const int * 不能把整型常量的地址给到普通指针
+    // int *_ptr = ptr;
+    int c = 20;
+      // 这样p就是一个常量指针，并且常量指针指向的东西也不能修改
+    // &p 表示的是 const int *const*
+    const int *p = &c;
+    const int*const* ptr = &p;
+    // 此时的const修饰的是 *ptr
+    cout << **ptr << endl;
+    // 相当于 const int ** <= int **，但是这是不成立的
+    // 二级指针 ptr 的 一级解引用 *ptr 和 p指向的是同一块内存空间
+    // 那么此时的 *ptr就是一个整型常量地址，然而这个整型常量地址又指向了普通指针p，所以这里是错误的
+    // ptr是一个指针，指向一个const指针，这个const指针指向了const int
     return 0;
 }
--- init.cpp
+++ init.cpp
@@ -1,20 +1,85 @@
+#include <filesystem>
+#include <type_traits>
 //
 // Created by huawei on 2026/5/29.
 //
+// 实现一个顺序栈
 #include<iostream>
 using namespace std;
-int gdata1 = 10;
-int gdata = 0;
-int gdata3;
-static int gdata4 = 11;
-static int gdata5 = 0;
-static int gdata6;
+class MyStack {
+public:
+    // 初始化
+    MyStack() {}
+    MyStack(int size = 10) {
+        cout << this << endl;
+        _stack = new int[size];
+        _top = -1;
+        _size = size;
+    }
+    // 析构函数 自动的释放资源
+    ~MyStack() {
+        cout << this << endl;
+        delete [] _stack;
+        _stack = nullptr;
+    }
+    void push(int x) {
+        if (is_full()) {
+            resize();
+        }
+        _stack[++_top] = x;
+    }
+    bool pop() {
+        if (is_empty()) {
+            return false;
+        }else {
+            --_top;
+            return true;
+        }
+    }
+    bool is_empty() {
+        return _top == -1;
+    }
+    //得到栈顶元素
+    int get_top() {
+        return _stack[_top];
+    }
+    // 判断栈是否溢出
+    bool is_full() {
+        return _top  == _size -1;
+    }
+    // 判断栈是否为空
+private:
+    int *_stack;
+    int _top;
+    int _size;
+    void resize() {
+        int *temp = new int[_size *2];
+        for (int i = 0; i < _size; i++) {
+            temp[i] = _stack[i];
+        }
+        delete [] _stack;
+        _stack = temp;
+    }
+};
+
 int main() {
-    int a = 12;
-    int b = 0;
-    int c;
-    static int e = 13;
-    static int f = 0;
-    static int g;
-    return 0;
+    MyStack *_my_stack = new MyStack(20);
+    _my_stack->push(1);
+    _my_stack->push(2);
+    _my_stack->push(3);
+    _my_stack->pop();
+    cout << _my_stack->get_top() << endl;
+    delete _my_stack;
+    // 先调用ps->~my_stack() 然后free(my_stack)
+
+    MyStack my_stack(20);
+    my_stack.push(10);
+    my_stack.push(20);
+    cout << my_stack.get_top() << endl;
+    my_stack.pop();
+    cout << my_stack.get_top() << endl;
+    my_stack.pop();
+    cout << my_stack.is_empty() << endl;
+   // 释放成员变量占用的外部堆内存
+    // cout << my_stack.get_top() << endl;
 }
